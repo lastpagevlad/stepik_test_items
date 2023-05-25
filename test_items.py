5f769808f094b8fe54469b71523ab8f8788d7990
Vlad <lastpagevlad@mail.ru>
2023-05-25 14:29:43 +0300

    Обновление метода

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 class TestProductPage:
 
-    def test_button_add_to_basket_is_visible(self, browser):
+    def test_items(self, browser):
         # Открываем страницу товара
         browser.get(link)
         # Установлено время принудительной задержки браузера
